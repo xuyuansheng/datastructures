f770d05cdbcff258b14a313e9ca67552dd1a37e8
浮躁的菜 <804288658@qq.com>
2020-03-07 18:06:51 +0800

    leetCode 问题1解答

--- src/main/java/com/java/study/datastructuresalgorithms/leetcode/QuestionOne.java
+++ src/main/java/com/java/study/datastructuresalgorithms/leetcode/QuestionOne.java
@@ -1,28 +1,40 @@
 package com.java.study.datastructuresalgorithms.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * 问题1:
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
- *
  * @author Mr.Xu
  * @date 2020/3/7 16:47
  */
 public class QuestionOne {
 
-    /*  给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
-你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
- */
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int[] ints = new QuestionOne().twoSum1(nums, 18);
+        System.out.println(Arrays.toString(ints));
+    }
 
     /**
      * @param nums
      * @param target
      * @return
      */
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum1(int[] nums, int target) {
 
-        return null;
+        int length = nums.length;
+        Map<Integer, Integer> map = new HashMap<>(length);
+        for (int i = 0; i < nums.length; i++) {
+            int second = target - nums[i];
+            if (map.containsKey(second)) {
+                return new int[]{map.get(second), i};
+            }
+            int current = (nums[i]);
+            map.put(current, i);
+        }
+        return new int[0];
     }
 
 }
