7a63696ffa03bb49c6c2afd40a9adc417aa0a699
浮躁的菜 <804288658@qq.com>
2020-03-19 21:50:27 +0800

    利用栈 实现类似浏览器前进后退的功能

--- src/main/java/com/java/study/datastructuresalgorithms/basisdatastructure/linear/stack/Stack.java
+++ src/main/java/com/java/study/datastructuresalgorithms/basisdatastructure/linear/stack/Stack.java
@@ -1,5 +1,7 @@
 package com.java.study.datastructuresalgorithms.basisdatastructure.linear.stack;
 
+import org.junit.Test;
+
 /**
  * 数据结构-栈
  *
@@ -7,4 +9,37 @@ package com.java.study.datastructuresalgorithms.basisdatastructure.linear.stack;
  * @date 2020/3/7 16:35
  */
 public class Stack {
+
+
+    /**
+     * 实现浏览器一样的前进后退功能
+     * 思路 : 使用两个链式栈,back 和 forward 每新进一个页面就把当前页面push到forward中
+     * 没后退一个页面,就把当前页面push到back中,并从forward中取出前一个页面
+     * 演示请见 fab.html
+     */
+    @Test
+    public void forwardAndBack() {
+
+
+        LinkedStack back = new LinkedStack();
+        LinkedStack forward = new LinkedStack();
+        String currentPage = "page1";
+        forward.push(currentPage);
+        currentPage = "page2";
+        forward.push(currentPage);
+        currentPage = "page3";
+        forward.push(currentPage);
+        currentPage = "page4";
+        forward.push(currentPage);
+        currentPage = "page5";
+        /*  前进了四个页面,开始后退 */
+        back.push(currentPage);
+        currentPage = forward.pop();
+        back.push(currentPage);
+        currentPage = forward.pop();
+        back.push(currentPage);
+        currentPage = forward.pop();
+
+    }
+
 }
