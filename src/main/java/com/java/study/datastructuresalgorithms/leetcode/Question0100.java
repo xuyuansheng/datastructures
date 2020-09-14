dbfc9dd169e8b55e7ad78c5fc1d54a28bb953fb4
root <root@kub22087.ch.com>
2020-09-14 18:31:21 +0800

    question0100

---
+++ src/main/java/com/java/study/datastructuresalgorithms/leetcode/Question0100.java
@@ -0,0 +1,120 @@
+package com.java.study.datastructuresalgorithms.leetcode;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Mr.Xu
+ * @date 2020/3/7 16:47
+ */
+public class Question0100 {
+
+
+    public static void main(String[] args) {
+        Integer[] nums = new Integer[]{2, 7, 11, 15};
+        Integer[] nums2 = new Integer[]{2, 7, 11, 16};
+        TreeNode build = Question0100.build(nums);
+        TreeNode build2 = Question0100.build(nums2);
+        boolean sameTree = new Question0100().isSameTree(build, build2);
+        System.out.println(sameTree);
+    }
+
+
+    /**
+     * 广度优先
+     * 时间复杂度O(min(m,n)),因为要遍历所有节点，所以时间复杂度为节点数比较少的那个数量
+     *
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        Queue<TreeNode> queueP = new LinkedList<>();
+        Queue<TreeNode> queueQ = new LinkedList<>();
+        queueP.offer(p);
+        queueQ.offer(q);
+        while (!queueQ.isEmpty() && !queueP.isEmpty()) {
+            TreeNode pollP = queueP.poll();
+            TreeNode pollQ = queueQ.poll();
+            if (pollP == null && pollQ == null) {
+                continue;
+            } else if (pollP == null || pollQ == null || pollP.val != pollQ.val) {
+                return false;
+            } else {
+                queueP.offer(pollP.left);
+                queueP.offer(pollP.right);
+                queueQ.offer(pollQ.left);
+                queueQ.offer(pollQ.right);
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * 深度优先,
+     * 时间复杂度O(min(m,n)),因为要遍历所有节点，所以时间复杂度为节点数比较少的那个数量
+     *
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTreeDfs(TreeNode p, TreeNode q) {
+        if (p == q) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else {
+            boolean res = p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            return res;
+        }
+    }
+
+    public static <T> TreeNode build(Integer[] t) {
+        TreeNode root = null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (t.length > 0) {
+            root = new TreeNode(t[0]);
+            queue.offer(root);
+        }
+        int index = 1;
+        while (!queue.isEmpty()) {
+            TreeNode poll = queue.poll();
+            if (index < t.length && t[index] != null) {
+                poll.left = new TreeNode(t[index]);
+                queue.offer(poll.left);
+            }
+            index += 1;
+            if (index < t.length && t[index] != null) {
+                poll.right = new TreeNode(t[index]);
+                queue.offer(poll.right);
+            }
+            index += 1;
+        }
+        return root;
+    }
+
+    @Data
+    @Accessors(chain = true)
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
